e21393ededd2f56d97e415cb482ebeef9150ae0f
Youssef Zaim <74555534+Youssef-Zaim@users.noreply.github.com>
2025-12-12 19:47:54 +0100

    Add test for rendering all todo items
    Updated tests to check rendering of todo items.

--- react-todo/src/components/TodoList.test.js
+++ react-todo/src/components/TodoList.test.js
@@ -1,48 +1,12 @@
 // src/components/TodoList.test.js
-import React from "react";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import "@testing-library/jest-dom";
 import TodoList from "./TodoList";
 
-describe("TodoList component", () => {
-  test("adds a todo", async () => {
-    const user = userEvent.setup();
-    render(<TodoList />);
+test("renders all todo items", () => {
+  const tasks = ["Task 1", "Task 2", "Task 3"];
+  render(<TodoList items={tasks} />);
 
-    const input = screen.getByLabelText("todo-input");
-    await user.type(input, "Test todo");
-    await user.click(screen.getByText("Add"));
-
-    expect(screen.getByText("Test todo")).toBeInTheDocument();
-  });
-
-  test("toggles a todo", async () => {
-    const user = userEvent.setup();
-    render(<TodoList />);
-
-    const input = screen.getByLabelText("todo-input");
-    await user.type(input, "Toggle me");
-    await user.click(screen.getByText("Add"));
-
-    const checkbox = screen.getByRole("checkbox");
-    expect(checkbox).not.toBeChecked();
-
-    await user.click(checkbox);
-    expect(checkbox).toBeChecked();
-  });
-
-  test("removes a todo", async () => {
-    const user = userEvent.setup();
-    render(<TodoList />);
-
-    const input = screen.getByLabelText("todo-input");
-    await user.type(input, "Delete me");
-    await user.click(screen.getByText("Add"));
-
-    const removeButton = screen.getByLabelText(/remove-/i);
-    await user.click(removeButton);
-
-    expect(screen.queryByText("Delete me")).not.toBeInTheDocument();
-  });
+  const listItems = screen.getAllByTestId("todo-item");
+  expect(listItems).toHaveLength(3);
 });
+
